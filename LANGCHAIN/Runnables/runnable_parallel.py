2e5404ec4798c8e5ef90c0973597243fe1bbb244
annu-soni-18 <annusoni7797@gmail.com>
2026-03-13 22:43:51 +0530

    Added runnable_parallel

--- LANGCHAIN/Runnables/runnable_parallel.py
+++ LANGCHAIN/Runnables/runnable_parallel.py
@@ -1,4 +1,26 @@
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableSequence
+from langchain_core.runnables import RunnableSequence, RunnableParallel
+
+model = ChatOllama(model="llama3")
+
+prompt1 = PromptTemplate(
+    template = "Generate a tweet about {topic}",
+    input_variables = ['topic']
+)
+
+prompt2 = PromptTemplate(
+    template = "Generate a LinkedIn post about {topic}",
+    input_variables = ['topic']
+)
+
+parser = StrOutputParser()
+
+chain = RunnableParallel({
+    'tweet': RunnableSequence(prompt1,model,parser),
+    'LinkedIn': RunnableSequence(prompt2,model,parser)
+})
+
+result = chain.invoke({'topic': 'LSTM'})
+print(result)
